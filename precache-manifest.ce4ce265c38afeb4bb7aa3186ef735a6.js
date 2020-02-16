571c7339634f35e62fec7c24c8bde479b68e6103
HarishReddy <urstrulyreddy017@gmail.com>
2020-02-16 19:07:29 +0530

    Updates

---
+++ precache-manifest.ce4ce265c38afeb4bb7aa3186ef735a6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7445f62dfa086e54659f0c868ff5308",
+    "url": "/HarishReddy1997/me/index.html"
+  },
+  {
+    "revision": "79d4a62f3f71107028a7",
+    "url": "/HarishReddy1997/me/static/css/2.bd81d9e0.chunk.css"
+  },
+  {
+    "revision": "18ef1384a4447d2980a1",
+    "url": "/HarishReddy1997/me/static/css/main.113e5579.chunk.css"
+  },
+  {
+    "revision": "79d4a62f3f71107028a7",
+    "url": "/HarishReddy1997/me/static/js/2.7e532867.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/HarishReddy1997/me/static/js/2.7e532867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ef1384a4447d2980a1",
+    "url": "/HarishReddy1997/me/static/js/main.eb26dca7.chunk.js"
+  },
+  {
+    "revision": "225b1e0527e02e3bdf2b",
+    "url": "/HarishReddy1997/me/static/js/runtime-main.2ff7fe02.js"
+  },
+  {
+    "revision": "f0d404d82bb8bc8c80c62747899fccb1",
+    "url": "/HarishReddy1997/me/static/media/background.f0d404d8.jpg"
+  },
+  {
+    "revision": "a88604907448261f8128f09cab0a4c79",
+    "url": "/HarishReddy1997/me/static/media/background1.a8860490.jpeg"
+  },
+  {
+    "revision": "4a75f5907635266f1c3d472ef2c94f47",
+    "url": "/HarishReddy1997/me/static/media/background2.4a75f590.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/HarishReddy1997/me/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/HarishReddy1997/me/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/HarishReddy1997/me/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/HarishReddy1997/me/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/HarishReddy1997/me/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4f0d87af3e44feee8649b54b033493e6",
+    "url": "/HarishReddy1997/me/static/media/profile.4f0d87af.jpeg"
+  }
+]);
